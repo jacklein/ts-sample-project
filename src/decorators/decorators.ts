c19c819e95751aeffa1984d57c4443ed1dc063d4
JK <jk@JKs-MacBook-Air.local>
2024-01-19 10:44:48 -0500

    setting up typescript project

---
+++ src/decorators/decorators.ts
@@ -0,0 +1,77 @@
+function Logger(logString: string) {
+    return function(constructor: Function){
+       console.log(logString);
+       console.log(constructor);
+    };
+ }
+ 
+ function WithTemplate(template: string, hookId: string){
+    return function(constructor: any){
+       console.log('rendering template');
+       const hookEl = document.getElementById(hookId);
+       const p = new constructor();
+       if (hookEl) {
+          hookEl.innerHTML = template
+          hookEl.querySelector('h1')!.textContent = p.name;
+       }
+    }
+ }
+ 
+ @Logger('LOGGING')
+ @WithTemplate('<h1>my person object</h1>','app')
+ class Person {
+    name = 'Max';
+ 
+    constructor() {
+       console.log('hello');
+    }
+ }
+ 
+ function Log(target: any, propertyName: string) {
+    console.log('Property decorator');
+    console.log(target, propertyName)
+ }
+ 
+ function Log2(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+ }
+ 
+ function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+ }
+ 
+ function Log4(target: any, name: string | Symbol, position: number){
+    console.log('Parameter decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+ }
+ 
+ class Product {
+    @Log
+    title: string;
+    private _price: number;
+ 
+    @Log2
+    set price(val: number) {
+       if (val > 0) {
+          this._price = val;
+       }
+    }
+ 
+    constructor(t: string, p: number) {
+       this.title = t;
+       this._price = p;
+    }
+ 
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+       return this._price * (1 + tax)
+    }
+ }
